b411ca49db7688036f5c32c654e32cf71fef98c6
Ruslan Rahimov <rahimoff.tr@gmail.com>
2025-05-30 18:33:12 +0300

    add service mobile css

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -6,7 +6,7 @@ const Services = () => {
 
   return (
     <div className="services-wrapper mt-15 w-11/12 mx-auto">
-      <h2 className="content-header font-raleway font-light text-lg text-charcoal-500 pt-8 uppercase tracking-widest mb-4">
+      <h2 className="content-header font-raleway font-light text-lg text-charcoal-500 pt-8 uppercase tracking-widest mb-4 max-md:pt-4">
         FİYAT LİSTESİ
       </h2>
       <div className="tabs flex gap-2 mb-4">
@@ -18,7 +18,7 @@ const Services = () => {
               key === activeTab
                 ? "bg-platinum-900 text-platinum-100"
                 : "bg-platinum-100 text-platinum-900"
-            }`}
+            } max-md:px-2 max-md:text-sm`}
           >
             {tabContent[key].title}
           </button>
@@ -28,16 +28,16 @@ const Services = () => {
         className="service-image-container my-10 h-96 rounded-xl bg-no-repeat bg-cover bg-center"
         style={{ backgroundImage: `url(${tabContent[activeTab].image})` }}
       ></div>
-      <ul className="service-list bg-chinese-500 my-4 p-8 rounded-xl">
-        <h2 className="text-xl">{tabContent[activeTab].title}</h2>
+      <ul className="service-list bg-chinese-500 my-4 p-8 rounded-xl max-md:p-3">
+        <h2 className="text-lg">{tabContent[activeTab].title}</h2>
         {tabContent[activeTab].services.map((service, index) => {
           return (
             <li key={index} className="flex bg-chinese-300 my-4 p-5 rounded-xl">
               <div className="left w-full">
-                <h3 className="text-lg">{service.name}</h3>
+                <h3 className="text-sm">{service.name}</h3>
               </div>
               <div className="right flex w-full justify-end">
-                <p className="price block">Uzman: {service.price} &#8378;</p>
+                <p className="price block text-sm">Uzman: {service.price} &#8378;</p>
               </div>
             </li>
           );
